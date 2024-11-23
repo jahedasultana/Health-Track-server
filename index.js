ece53a3cd231255a653f0f05d7112c9105d357f1
jaheda8256 <jahedasultana47@gmail.com>
2024-11-23 16:33:37 +0600

    server setup done

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // Middleware
 app.use(express.json()); // To parse JSON request bodies
 app.use(cors());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wotzmkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
 // Routes
 app.get('/', (req, res) => {
   res.send('Welcome to My Express App!');
