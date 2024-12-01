6d28e2e8e17214046af29b8cd230b8a5f191c149
jaheda8256 <jahedasultana47@gmail.com>
2024-12-01 09:12:33 +0600

    change border color

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(express.json()); // To parse JSON request bodies
 app.use(cors());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wotzmkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +23,7 @@ async function run() {
   try {
 
     const userCollections = client.db('Health-Track').collection('users');
+    const doctorsCollections = client.db('Health-Track').collection('doctors');
 
     app.post("/userCreate", async(req,res) =>{
       const body = req.body;
