249f89ac0ca87fd3eb98912a6eb9176997c0366b
jaheda8256 <jahedasultana47@gmail.com>
2024-12-05 23:38:33 +0600

    added new api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // Middleware
 app.use(express.json()); // To parse JSON request bodies
 app.use(cors());
@@ -42,12 +42,45 @@ async function run() {
     })
 
     
+// API route to get only doctors
+app.get("/doctors", async (req, res) => {
+  try {
+    const doctors = await userCollections.find({ role: "doctor" }).toArray();
+    res.status(200).json(doctors);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data", error });
+  }
+});
+
+// API route to get a single doctor by ID
+app.get("/doctors/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const doctor = await userCollections.findOne({ _id: new ObjectId(id), role: "doctor" });
+    if (!doctor) {
+      return res.status(404).json({ message: "Doctor not found" });
+    }
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching doctor", error });
+  }
+});
+
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     
   }
 }
 run().catch(console.dir);
+
+
+
+
 
 
 // Routes
